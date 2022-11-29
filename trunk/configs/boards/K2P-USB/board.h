c8ff96fdace071dc557b40b900d41d3ed8579c3e
shifoxiao <119347786+shifoxiao@users.noreply.github.com>
2022-11-29 13:51:59 +0800

    Update board.h

--- trunk/configs/boards/K2P-USB/board.h
+++ trunk/configs/boards/K2P-USB/board.h
@@ -8,6 +8,19 @@
 #define BOARD_MODEL_URL		"http://www.phicomm.com/"
 #define BOARD_BOOT_TIME		30
 #define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET        12
+#define BOARD_GPIO_BTN_WPS        18
+#define BOARD_GPIO_BTN_FN1        14
+#define BOARD_GPIO_LED_WIFI        5
+
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_POWER
+#undef  BOARD_GPIO_LED_LAN
+#undef  BOARD_GPIO_LED_WAN
+#undef  BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB_ON
+#undef  BOARD_GPIO_PWR_USB
 
 #define BOARD_HAS_5G_11AC		1
 #define BOARD_NUM_ANT_5G_TX		2
